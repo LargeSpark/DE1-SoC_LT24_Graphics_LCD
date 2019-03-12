3c96561bd6627181eb2bf5ececfaf9265e3ee660
Alex <alex@hectospark.co.uk>
2019-03-12 13:41:37 +0000

    Fixed Cube fault
    Still need to fix circle outline fault

--- Graphics/graphics.c
+++ Graphics/graphics.c
@@ -4,6 +4,59 @@ void Graphics_initialise(unsigned volatile int lcd_pio_base,unsigned volatile in
 }
 
 void Graphics_drawBox(unsigned int x1,unsigned int y1,unsigned int x2,unsigned int y2,unsigned short colour,bool noFill,unsigned short fillColour){
+	int sx1 = (int) x1;
+	int sx2 = (int) x2;
+	int sy1 = (int) y1;
+	int sy2 = (int) y2;
+	int height = abs(sy2-sy1);
+	int width = abs(sx1-sx2);
+	int y=0;
+	int x=0;
+	int oy=0;
+	int ox=0;
+
+	//find bottom left value
+	int llx = 0;
+	int lly = 0;
+	if(sx1<sx2){
+		llx = sx1;
+	}
+	else{
+		llx = sx2;
+	}
+
+	if(sy1<sy2){
+		lly = sy1;
+	}
+	else{
+		lly = sy2;
+	}
+
+	//cube fill (draws first so it can be overdrawn with outline)
+	if(~noFill){
+		for(y=0; y <= height; y++){
+			for(x=0; x<=width; x++){
+				LT24_drawPixel(fillColour,x+llx,y+lly);
+			}
+		}
+	}
+
+	//cube outline
+	//verticle outline
+	for(oy = 0; oy <=height; oy++){
+		LT24_drawPixel(colour,sx1,lly+oy);
+	}
+	for(oy = 0; oy <=height; oy++){
+		LT24_drawPixel(colour,sx2,lly+oy);
+	}
+	//horizontal outline
+	for(ox = 0; ox <=width; ox++){
+		LT24_drawPixel(colour,llx+ox,sy1);
+	}
+	for(ox = 0; ox <=width; ox++){
+		LT24_drawPixel(colour,llx+ox,sy2);
+	}
+
 
 }
 
@@ -13,7 +66,7 @@ void Graphics_drawCircle(unsigned int x,unsigned int y,unsigned int r,unsigned s
 	//Radius squared
 	int rad2 = signedr * signedr;
 	//Outline threshold
-	int outThres = 100;
+	int outThres = 300;
 	//Go through x's
 	int xc = 0;
 	int yc = 0;
